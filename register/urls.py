e16e3bd8fbd77994bca9ac032ad6a30e306acb0b
saqib-devops <saqibahmad778866@gmail.com>
2023-04-07 15:41:50 -0400

    Register: login form added

--- register/urls.py
+++ register/urls.py
@@ -4,7 +4,7 @@ app_name = 'register'
 from django.contrib.auth import views as auth_views
 from django.urls import path
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='register/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', signup, name='signup'),
 
